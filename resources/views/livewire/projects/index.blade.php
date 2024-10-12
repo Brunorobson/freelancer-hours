26c1302fd1824c5f57587dcd47bd61a6b0a21552
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2024-10-12 15:51:36 -0300

    nova proposta enviada

--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -1,8 +1,7 @@
 <div class="grid grid-cols-2 gap-4">
     @foreach ($this->projects as $project)
-            <a href="{{ route('projects.show', $project) }}">
-                <x-project-card-simple :$project />
-
-            </a>
+        <a href="{{ route('projects.show', $project) }}">
+            <x-projects.simple-card :$project />
+        </a>
     @endforeach
 </div>
